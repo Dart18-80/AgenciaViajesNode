c5df5cf5dc0c2d6dea388b6730e1f03fea0bf366
Dart18-80 <diegoruiztrabanino13@gmail.com>
2021-12-17 16:42:08 -0600

    Pagina de testimoniales con base de datos completa

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,6 +1,45 @@
+import { Testimonial } from "../models/Testimoniales.js";
 
-const guardarTestimonial = (req, res) =>{// req - lo que enviamos : res - lo que express nos responde
-    console.log(req.body);
+const guardarTestimonial = async (req, res) =>{// req - lo que enviamos : res - lo que express nos responde
+    const { nombre, correo, mensaje } = req.body;
+    const errores = [];
+
+    if(nombre.trim() === ''){
+        errores.push({mensaje: 'El nombre esta vacio'});
+    }
+    if(correo.trim() === ''){
+        errores.push({mensaje: 'El correo esta vacio'});
+    }
+    if(mensaje.trim() === ''){
+        errores.push({mensaje: 'El mensaje esta vacio'});
+    }
+
+    if(errores.length > 0){
+
+        //Consultar testimoniales existentes 
+        const testimoniales = await Testimonial.findAll();
+
+        res.render('testimoniales', {
+            pagina: 'Testimoniales', 
+            errores, 
+            nombre, 
+            correo, 
+            mensaje,
+            testimoniales
+        })
+    }else{
+        //Almacenarlo en la base de datos
+        try {
+            await Testimonial.create({ 
+                nombre, 
+                correo, 
+                mensaje
+            });
+            res.redirect('/testimoniales');
+        } catch (error) {
+            
+        }
+    }
 }
 
 export { guardarTestimonial };
